d3ef5ec0452449728a6a1b256c1c98763b35dda6
kubenet <kubenet@gmail.com>
2014-04-22 02:22:07 +0700

    Пример работы с usb клавиатурой

---
+++ KeyboardHelloWorld.ino
@@ -0,0 +1,47 @@
+#include <HIDKeyboard.h>
+
+HIDKeyboard keyboard; // Initialize HIDKeyboard object
+
+void setup() {
+  keyboard.begin(); // Start communication
+  delay(2000); // Wait for device to be found as a keyboard
+}
+
+void loop() {
+
+  // Эмулируем нажатие клавишь: Вверх, Вниз, Вправо, Влево
+  
+  keyboard.pressSpecialKey(RIGHTARROW); // Notice use of pressSpecialKey for ENTER
+  keyboard.releaseKey();
+  delay(100);
+  keyboard.pressSpecialKey(UPARROW); // Notice use of pressSpecialKey for ENTER
+  keyboard.releaseKey();
+  delay(100);
+  keyboard.pressSpecialKey(RIGHTARROW); // Notice use of pressSpecialKey for ENTER
+  keyboard.releaseKey();
+  delay(100);
+  keyboard.pressSpecialKey(UPARROW); // Notice use of pressSpecialKey for ENTER
+  keyboard.releaseKey();
+  delay(100);
+  keyboard.pressSpecialKey(UPARROW); // Notice use of pressSpecialKey for ENTER
+  keyboard.releaseKey();
+  delay(100);
+  keyboard.pressSpecialKey(LEFTARROW); // Notice use of pressSpecialKey for ENTER
+  keyboard.releaseKey();
+  delay(100);
+  keyboard.pressSpecialKey(UPARROW); // Notice use of pressSpecialKey for ENTER
+  keyboard.releaseKey();
+  delay(100);
+  keyboard.pressSpecialKey(LEFTARROW); // Notice use of pressSpecialKey for ENTER
+  keyboard.releaseKey();
+  delay(100);
+  keyboard.pressSpecialKey(UPARROW); // Notice use of pressSpecialKey for ENTER
+  keyboard.releaseKey();
+  delay(100);
+  keyboard.pressSpecialKey(LEFTARROW); // Notice use of pressSpecialKey for ENTER
+  keyboard.releaseKey();
+  delay(100);
+  keyboard.pressSpecialKey(UPARROW); // Notice use of pressSpecialKey for ENTER
+  keyboard.releaseKey();
+  delay(100);
+}
